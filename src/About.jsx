26a8d8522ffed7da3bfcc3caaf103671b8307135
Krish Kasavala <krrishkasvala@gmail.com>
2026-06-07 14:51:15 +0530

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <div>
+            <h2>welcome to About page</h2>
+        </div>
+    );
+}
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+function Hello({ data }) {
+  return (
+    <div>
+      <h1>Hello, {data.name}!</h1>
+    </div>
+  );    
+}
+
+export default Hello;
